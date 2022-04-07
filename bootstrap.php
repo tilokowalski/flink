d6db74fe08f19a6871dbd0301755efd5c3e9e115
Tilo Kowalski <privat@tilokowalski.de>
2022-04-07 19:29:55 +0200

    improved

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+    // ERROR HANDLING CONFIGURATION
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+
+    error_reporting(E_ALL);
+
+    // AUTOMATIC CLASS INCLUSION
+
     spl_autoload_register('flink_autoload');
     function flink_autoload($class_name) {
         $result = '';
--- src/Assert.php
+++ src/Assert.php
@@ -4,27 +4,26 @@ class Flink_Assert {
 
     public static function instanceof($object, $class, ?string $message = null) {
         if (!$object instanceof $class) throw new Flink_Exception_AssertionFailed($message);
-        return true;
     }
 
     public static function equals($x, $y, ?string $message = null) {
         if ($x !== $y) throw new Flink_Exception_AssertionFailed($message);
-        return true;
     }
 
     public static function not_equals($x, $y, ?string $message = null) {
         if ($x === $y) throw new Flink_Exception_AssertionFailed($message);
-        return true;
     }
 
     public static function in_array($value, $array, ?string $message = null) {
         if (!in_array($value, $array)) throw new Flink_Exception_AssertionFailed($message);
-        return true;
     }
 
     public static function array_key_exists($key, $array, ?string $message = null) {
         if (!array_key_exists($key, $array)) throw new Flink_Exception_AssertionFailed($message);
-        return true;
+    }
+
+    public static function file_exists($path, ?string $message = null) {
+        if (!file_exists($path)) throw new Flink_Exception_AssertionFailed($message);
     }
 
 }
