bdb4126e52d56113ae470fe2fb505c740fee9811
Tilo Kowalski <privat@tilokowalski.de>
2022-04-07 19:30:11 +0200

    improved

--- src/Exception/ViewComponent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Flink_Exception_ViewComponent extends Flink_Exception {
-
-    public function __construct(?string $message = null) {
-        if (null === $message) $message = 'failed to handle view component';
-        parent::__construct($message);
-    }
-}
