07fa1860d04c7a8e5c779b999db1d84833ad5b70
Tilo Kowalski <tilokowalski@Air-von-Tilo.fritz.box>
2022-06-09 23:02:30 +0200

    recaptcha

--- src/ViewComponent/FormElement/GoogleRecaptcha.php
+++ src/ViewComponent/FormElement/GoogleRecaptcha.php
@@ -5,6 +5,8 @@ class Flink_ViewComponent_FormElement_GoogleRecaptcha extends Flink_ViewComponen
     private $key;
     private $secret;
 
+    private $response;
+
     public function set_key(string $key): self {
         $this->key = $key;
         return $this;
@@ -23,31 +25,38 @@ class Flink_ViewComponent_FormElement_GoogleRecaptcha extends Flink_ViewComponen
         return $this->secret;
     }
 
+    public function get_response() {
+        if (null === $this->response) {
+            Flink_Assert::is_true($this->get_form()->is_submitted(), 'captcha success validation called before form submission');
+            Flink_Assert::is_false($this->is_empty());
+    
+            $data = array(
+                'secret' => $this->get_secret(),
+                'response' => $this->get_value()
+            );
+            $query = http_build_query($data);
+    
+            $options = array(
+                'http' => array (
+                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n", 
+                        "Content-Length: " . strlen($query) . "\r\n" .
+                        "User-Agent:MyAgent/1.0\r\n",
+                    'method' => 'POST',
+                    'content' => $query
+                )
+            );
+    
+            $context = stream_context_create($options);
+            $verify = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+            $this->response = json_decode($verify);
+        }
+        return $this->response;
+    }
+
     public function was_successful() {
-        Flink_Assert::is_true($this->get_form()->is_submitted(), 'captcha success validation called before form submission');
-        if (Flink_Application::is_localhost()) return true;
-        if ($this->is_empty()) return false;
-                
-        $data = array(
-            'secret' => $this->get_secret(),
-            'response' => $this->get_value()
-        );
-        $query = http_build_query($data);
-
-        $options = array(
-            'http' => array (
-                'header' => "Content-Type: application/x-www-form-urlencoded\r\n", 
-                    "Content-Length: " . strlen($query) . "\r\n" .
-                    "User-Agent:MyAgent/1.0\r\n",
-                'method' => 'POST',
-                'content' => $query
-            )
-        );
-
-        $context = stream_context_create($options);
-        $verify = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
-
-        return json_decode($verify)->success;
+        // if (Flink_Application::is_localhost()) return true;
+        return $this->get_response()->success;
+        
     }
 
     public function render() {
