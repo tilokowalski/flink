11ff678868029850c4bbdf45005cc1388ad6a452
Tilo Kowalski <tilokowalski@MacBook-Air-von-Tilo.fritz.box>
2022-06-25 13:42:42 +0200

    cookies

--- src/Application.php
+++ src/Application.php
@@ -4,8 +4,6 @@ class Flink_Application {
 
     private $connection;
 
-    public static $jquery_included = false;
-
     public function connect_db(string $hostname, string $username, string $password, string $database) {
         $connection = new Flink_Connection($hostname, $username, $password, $database);
         if ($connection->connect_error) throw new Flink_Exception_Database('connection failed');
@@ -26,21 +24,12 @@ class Flink_Application {
     }
 
     public static function set_cookie($name, $value, $expiration, ?string $path = '/') {
-        self::include_jquery_if_needed();
         echo "<script>$.cookie('" . $name . "', '" . $value . "', { expires: " . $expiration . ", path: '" . $path . "' })</script>";
     }
 
     public static function remove_cookie($name, ?string $path = '/') {
         if (!isset($_COOKIE[$name])) return;
-        self::include_jquery_if_needed();
         echo "<script>$.removeCookie('" . $name . "', { path: '" . $path . "' })</script>";
-    }
-
-    private static function include_jquery_if_needed() {
-        if (self::$jquery_included) return;
-        echo "<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>";
-        echo "<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js'></script>";
-        self::$jquery_included = true;
     }
 
     public static function prepare_url($url) {
