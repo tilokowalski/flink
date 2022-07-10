386a8b3e994bc703d6d0513f11079d8710105488
Tilo Kowalski <tilokowalski@MacBook-Air-von-Tilo.local>
2022-07-10 12:20:06 +0200

    toast vc completed

--- src/ViewComponent/Toast.php
+++ src/ViewComponent/Toast.php
@@ -7,15 +7,27 @@ class Flink_ViewComponent_Toast extends Flink_ViewComponent {
     const TYPE_ERROR = 'error';
     const TYPE_INFO = 'info';
 
+    const POSITION_TOP_LEFT = 'top-left';
+    const POSITION_TOP_RIGHT = 'top-right';
+    const POSITION_TOP_CENTER = 'top-center';
+    const POSITION_BOTTOM_LEFT = 'bottom-left';
+    const POSITION_BOTTOM_RIGHT = 'bottom-right';
+    const POSITION_BOTTOM_CENTER = 'bottom-center';
+
+    public static $default_position = self::POSITION_TOP_RIGHT;
+    public static $default_autoclose = 5000;
+
     private $type;
     private $title;
     private $message;
+
+    private $position;
+    private $autoclose;
 
     public function __construct(string $type, string $title, string $message) {
         $this->type = $type;
         $this->title = $title;
         $this->message = $message;
-        parent::__construct('toast');
     }
 
     public function get_type() {
@@ -30,20 +42,57 @@ class Flink_ViewComponent_Toast extends Flink_ViewComponent {
         return $this->message;
     }
 
+    public function get_position() {
+        if (null !== $this->position) return $this->position;
+        return self::$default_position;
+    }
+
+    public function get_autoclose() {
+        if (null !== $this->autoclose) return $this->autoclose;
+        return self::$default_autoclose;
+    }
+
+    public function set_position(string $position) {
+        Flink_Assert::in_array($position, [self::POSITION_TOP_LEFT, self::POSITION_TOP_RIGHT, self::POSITION_TOP_CENTER, self::POSITION_BOTTOM_LEFT, self::POSITION_BOTTOM_RIGHT, self::POSITION_BOTTOM_CENTER], $position . ' is not a valid position');
+        $this->position = $position;
+        return $this;
+    }
+
+    public function set_autoclose(int $autoclose) {
+        $this->autoclose = $autoclose;
+        return $this;
+    }
+
+    public function prepare() {
+        $_SESSION['toasts'][] = serialize($this);
+    }
+
+    public static function prepared_exists() {
+        return isset($_SESSION['toasts']) && !empty($_SESSION['toasts']);
+    }
+
+    public static function show_prepared() {
+        foreach ($_SESSION['toasts'] as $key => $toast) {
+            $toast = unserialize($toast);
+            $toast->render();
+            unset($_SESSION['toasts'][$key]);
+        }
+    }
+
     public static function success($title, $message) {
-        (new self(self::TYPE_SUCCESS, $title, $message))->render();
+        return new self(self::TYPE_SUCCESS, $title, $message);
     }
 
     public static function warning($title, $message) {
-        (new self(self::TYPE_WARNING, $title, $message))->render();
+        return new self(self::TYPE_WARNING, $title, $message);
     }
 
     public static function error($title, $message) {
-        (new self(self::TYPE_ERROR, $title, $message))->render();
+        return new self(self::TYPE_ERROR, $title, $message);
     }
 
     public static function info($title, $message) {
-        (new self(self::TYPE_INFO, $title, $message))->render();
+        return new self(self::TYPE_INFO, $title, $message);
     }
-    
+
 }
