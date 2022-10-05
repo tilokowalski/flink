007d5987f8b9e3c373bf7ab17f7dd7c6e253251b
Tilo Kowalski <tilokowalski@MacBook-Air-von-Tilo.fritz.box>
2022-10-05 10:37:50 +0200

    values also for text areas

--- src/ViewComponent/FormElement/Textarea.php
+++ src/ViewComponent/FormElement/Textarea.php
@@ -2,4 +2,15 @@
 
 class Flink_ViewComponent_FormElement_Textarea extends Flink_ViewComponent_FormElement {
 
+    private $value;
+
+    public function set_value(?string $value = null) {
+        $this->value = $value;
+        return $this;
+    }
+
+    public function get_value() {
+        return $this->value;
+    }
+
 }
