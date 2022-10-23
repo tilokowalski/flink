239b71501e1020523fc0df3598cb5817a2efdde8
Tilo Kowalski <tilokowalski@TK-MacBookAir.fritz.box>
2022-10-23 20:42:05 +0200

    composer update

---
+++ src/Exception.php
@@ -0,0 +1,5 @@
+<?php
+
+abstract class Flink_Exception extends Exception {
+    
+}
--- src/Exception/Database.php
+++ src/Exception/Database.php
@@ -1,6 +1,6 @@
 <?php
 
-class Flink_Exception_Database extends Delight_Exception {
+class Flink_Exception_Database extends Flink_Exception {
 
     public function __construct(?string $message = null) {
         if (null === $message) $message = 'failed to handle database';
