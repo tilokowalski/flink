be21b054ec758edabba4456beafd0f6d791d4634
Tilo Kowalski <privat@tilokowalski.de>
2022-12-01 17:44:10 +0100

    dates

--- src/Database/TypeConverter.php
+++ src/Database/TypeConverter.php
@@ -32,6 +32,7 @@ class Flink_Database_TypeConverter {
         if (is_int($value)) return strval($value);
         if (is_bool($value)) return boolval($value) ? '1' : '0';
         if (is_string($value)) return htmlentities($value);
+        if ($value instanceof DateTime) return date_format($value, 'Y.m.d');
         throw new Delight_Exception_NotImplemented('type converter has no implementation for conversion of ' . $value);
     }
 
